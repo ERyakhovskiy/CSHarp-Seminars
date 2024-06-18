2a218156a6063eb5b39d3d403bcb475d00600ff3
Evgeniy Ryakovskiy <ryakhovskii87@gmail.com>
2024-06-17 23:54:46 -0300

    Добавил проект в Task1

--- homework/dz2/Task1/Program.cs
+++ homework/dz2/Task1/Program.cs
@@ -2,10 +2,16 @@
 // Напишите программу, которая принимает на вход число и проверяет, 
 // кратно ли оно ддновременно 7 и 23
 
-int a = 322;
+Console.Write("Input a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
 if (a % 7 == 0 && a % 23 == 0)
 {
-    Console.WriteLine("Да");
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No")
 }
 
 
