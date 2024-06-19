b4f6ad60fd826e0482608536147a84172f88ebb5
Evgeniy Ryakovskiy <ryakhovskii87@gmail.com>
2024-06-18 22:08:41 -0300

    Solved seminar3

---
+++ Seminars/sem3/Task2/Program.cs
@@ -0,0 +1,51 @@
+﻿// ЗАДАНИЕ №2
+
+// Задайте массив на 10 элементов, заполеннный числами из
+// промежутка [-10, 10]. Замените отрицательные элементы на
+// положительные, а положительные на отрицательные.
+// Прмер:
+// [1 -5 6] => [-1 5 -6]
+
+// РЕШЕНИЕ:
+
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+int[] ChangeArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+    return array;
+}
+
+System.Console.WriteLine("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input minimal valey of array element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input maximal valey of array element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(size, min, max);
+PrintArray(myArray);
+
+myArray = ChangeArray(myArray);
+PrintArray(myArray);
---
+++ Seminars/sem3/Task3/Program.cs
@@ -0,0 +1,55 @@
+﻿// ЗАДАНИЕ №3
+
+// Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний,
+// и т.д. Результаты запишите в новый массив.
+
+// ПРИМЕР:
+// [1 3 2 4 2 3] => [3 6 8]
+// [2 3 1 7 5 6 3] => [6 18 5]
+
+// РЕШЕНИЕ:
+
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(min, max + 1);
+    }
+    return array;
+}
+
+int[] CompositionArray(int[] array)
+{
+    int[] resultArray = new int[array.Length / 2];
+    for(int i = 0; i < resultArray.Length; i++)
+    {
+        resultArray[i] = array[i] * array[array.Length - i - 1];
+    }
+    return resultArray;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+System.Console.WriteLine("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input minimal valey of array element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input maximal valey of array element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(size, min, max);
+PrintArray(myArray);
+
+PrintArray(CompositionArray(myArray));
+
+
