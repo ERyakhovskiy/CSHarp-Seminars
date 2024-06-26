f50c7c69b302a81385175d057da6a336e8645508
Evgeniy Ryakovskiy <ryakhovskii87@gmail.com>
2024-06-25 23:25:20 -0300

    add metods & task1 of 5seminar

---
+++ Seminars/sem5/Program.cs
@@ -0,0 +1,38 @@
+﻿// Семинар 5 Двумерные Масивы.
+// Методы:
+// Создание и заполднение двумерного Массива.
+
+int[,] CreateRandomMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random random = new Random();
+    for(int i = 0; i < rows; i++)// прохождение по столбцу(сверху вниз)
+    {
+        for(int j = 0; j < columns; j++) // прохождение по строке(слева на право)
+        {
+            matrix[i, j] = random.Next(min, max + 1);// генирирует целочисленное значение в заданном диапазоне
+        }
+    }
+    return matrix;
+}
+// 0 8 9 6
+// 1 2 7 4
+// 9 4 1 6
+
+
+// Метод PrintMatrix
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)// прохождение по столбцу(сверху вниз)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++) // прохождение по строке(слева на право)
+        {
+            Console.Write(matrix[i, j] + " ", ConsoleColor.Green);// вывод строки в консоль
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintMatrix(CreateRandomMatrix(4,6,0,9));  // выводит двумерный Массив(кол-во строк, кол-во столбцов, мин эл-т., макс. эл.)
+
---
+++ Seminars/sem5/task1/Program.cs
@@ -0,0 +1,74 @@
+﻿// ЗАДАЧА №1
+// Задайте двумерный массив. Найдите элементы, у которых
+// оба индекса четные, и замените эти эл-ты на их квадраты.
+// Пример:
+// 2 3 4 3
+// 4 3 4 1 =>
+// 2 9 5 4 
+
+// 4 3 16 3
+// 4 3 4 1
+// 4 9 25 4
+
+// Решение:
+
+int[,] EvenNumsToQuad(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)// прохождение по столбцу(сверху вниз)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++) // прохождение по строке(слева на право)
+        {
+            if(i % 2 == 0 && j % 2 == 0) // отлавливаем все эл-ты у которых оба индекса четные
+            {
+                matrix[i, j] = matrix[i, j] * matrix[i, j]; // подменяем значение эл-та на его квадрат
+            }
+        }
+           
+    }
+    return matrix;
+}
+
+
+
+int[,] CreateRandomMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random random = new Random();
+    for(int i = 0; i < rows; i++)// прохождение по столбцу(сверху вниз)
+    {
+        for(int j = 0; j < columns; j++) // прохождение по строке(слева на право)
+        {
+            matrix[i, j] = random.Next(min, max + 1);// генирирует целочисленное значение в заданном диапазоне
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)// прохождение по столбцу(сверху вниз)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++) // прохождение по строке(слева на право)
+        {
+            Console.Write(matrix[i, j] + " ", ConsoleColor.Green);// вывод строки в консоль
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+System.Console.WriteLine("Input number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input min of array: ");
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input max of array: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] matrix = CreateRandomMatrix(rows, columns, min, max); // матрицу созданную функцией CreateMatrix запишем в переменную int[,] matrix
+PrintMatrix(matrix); // выводит созданную матрицу
+
+matrix = EvenNumsToQuad(matrix); // перезапишем созданную матрицу измененной(Эл-ты которой возвели в квадрат)
+PrintMatrix(matrix);
