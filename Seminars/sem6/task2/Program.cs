b487cc39c41b0224e06d9ed8ccbabb12a4abfb8f
Evgeniy Ryakovskiy <ryakhovskii87@gmail.com>
2024-07-11 23:14:45 -0300

    Solved task2 & task3 in seminar6

--- Seminars/sem6/task2/Program.cs
+++ Seminars/sem6/task2/Program.cs
@@ -1,2 +1,41 @@
-﻿Задание №2
+﻿// Задание №2
+// На основе символов строки (тип string) сформировать массив
+// символов (тип char[]). Вывести массив на экран.
+// Указание
+// Метод строки ToCharArray() не использовать.
+// Пример
+// “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ] 
 
+
+string CharsToStrings(char[] chars)
+{
+    string result = string.Empty; // объявили переменную result и присвоили ей значение "пусто"
+    for(int i = 0; i < chars.Length; i++)
+    {
+        result += chars[i]; // проходим по элементам массива и каждый из них приписываем строке result
+    }
+    return result; // возвращаем метод
+}   
+
+
+
+char[] StringToChars(string str)
+{
+    char[] result = new char[str.Length]; // создали пустой массив
+    for(int i = 0; i < str.Length; i++)
+    {
+        result [i] = str[i]; // каждому эл-ту [i] массива result присваимваем [i] значение строки  
+    }
+    return result; // вернем переписанный массив
+}
+
+void PrintArray(char[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write("'" + array[i] + "' "); // добавили к значению эл-та массива (') спереди и (') сзади + пробел(при выводе на консоль)
+    }
+    System.Console.WriteLine("");
+}
+
+PrintArray(StringToChars("abcdef"));
